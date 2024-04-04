454937827cde396d800fb39f5f5869db649b6cd0
Squirrel-red <elena.spb@sfr.fr>
2024-04-04 22:38:34 +0200

    Correction des bugs et ajout style

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,38 @@
-    // on ajoute un écouteur d'événement sur le bouton pour appeler une fonction ajout
-    const addBtn = document.querySelector('#btn');
-    addBtn.addEventListener('click', addTask);            // add à task on click
+    // 
+    const btn = document.querySelector('#btn');
+    const todoCard = document.querySelector(".todoCard");
+    const todoCards = document.querySelector("#todoCards");
+    const compteurDiv = document.querySelector(".compteur");
 
-    // on créé la fonction d'ajout
-    // const addBtn = document.querySelector('#btn');
-    const taskCard = document.querySelector(".todoCard");
-    const tasksContainer = document.querySelector("#todoCards");
+    const classTodoCards = todoCards.classList.contains("todoCards")
+
+    let compteur = 1;
+    compteurDiv.innerHTML = compteur;
+
+// Ajoute une nouvelle card
+btn.addEventListener("click", function(){
+    const cloneCard = todoCard.cloneNode(true);
+    todoCards.appendChild(cloneCard)
+    compteur++;
+    compteurDiv.innerHTML = compteur;
+
+})
+
+ //Supprime une card
 
 
-    function addTask(){
-        const newTask = taskCard.cloneNode(true)          // clone the  task card
-        const newDelBtn = newTask.querySelector('delBtn')
-        const newTextArea = newTask.querySelector('.task')
+ 
+todoCards.addEventListener("click", function(event){
+    const targetElement = event.target;
+   console.log(targetElement.closest(".delBtn"))
+    if(targetElement.classList.contains("fa")){
+    const btnSupprimer = targetElement.closest(".delBtn")
 
-        newTextArea.value = "New Task"                    // set new task text to "New Task"
-        newDelBtn.addEventListener('click', function() {  // add delete event listener to new task
-            deleteTask(newTask);                          // target the new task
-        });
-        tasksContainer.appendChild(newTask)               // append new task to the tasks container
-        updateCount();
-    }
-
-    // on ajout un écouteur d'évenement sur le bouton pour appeler une fonction suppression
-    const delBtn = document.querySelector('.delBtn');
-    delBtn.addEventListener('click', function() {         // delete default task on click  
-        deleteTask(taskCard);                             // target the right task
-    });
-
-    // on créé la fonction de suppression
-    function deleteTask(task){
-        task.remove();                                    // remove the task
-    }
+           const parentButtonSuppr = btnSupprimer.parentElement
+           parentButtonSuppr.remove()
+           
+           compteur--;
+           compteurDiv.innerHTML = compteur;
+       }
+       console.log("ok")
+})
