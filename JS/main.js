3eed23993d0a3d6fc4bbe5896ee02681048d2225
Squirrel-red <elena.spb@sfr.fr>
2024-04-05 16:19:06 +0200

    Corrections finales

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,38 @@
-    // 
+    // Déclaration de constantes
     const btn = document.querySelector('#btn');
     const todoCard = document.querySelector(".todoCard");
     const todoCards = document.querySelector("#todoCards");
-    const compteurDiv = document.querySelector(".compteur");
+    const compteurNombreCards = document.querySelector(".compteur");
+  //const classTodoCards = todoCards.classList.contains("todoCards")
 
-    const classTodoCards = todoCards.classList.contains("todoCards")
-
+    // Déclaration de la variable liée au div compteur dans index.html
     let compteur = 1;
-    compteurDiv.innerHTML = compteur;
+    compteurNombreCards.innerHTML = compteur;
 
-// Ajoute une nouvelle card
+// Ajoute un nouveau task card
 btn.addEventListener("click", function(){
-    const cloneCard = todoCard.cloneNode(true);
-    todoCards.appendChild(cloneCard)
-    compteur++;
-    compteurDiv.innerHTML = compteur;
+    const cloneCard = todoCard.cloneNode(true);     // on clone le task card
+    const cloneTextArea = cloneCard.querySelector('.task');
+    cloneTextArea.value = "New Task";               // pour créer  le nouveau task card vierge et ne pas copier le contenu de task précédent
+    todoCards.appendChild(cloneCard)                // on inscrit  le nouveau task card au container des cards
+
+    compteur++;                                     //on incrémente le compteur et on le mis à jour
+    compteurNombreCards.innerHTML = compteur;
 
 })
 
- //Supprime une card
-
-
- 
+//Supprime un task card
 todoCards.addEventListener("click", function(event){
     const targetElement = event.target;
-   console.log(targetElement.closest(".delBtn"))
+    console.log(targetElement.closest(".delBtn"))
     if(targetElement.classList.contains("fa")){
     const btnSupprimer = targetElement.closest(".delBtn")
 
            const parentButtonSuppr = btnSupprimer.parentElement
            parentButtonSuppr.remove()
            
-           compteur--;
-           compteurDiv.innerHTML = compteur;
+           compteur--;                               //on décrémente le compteur et on le mis à jour
+           compteurNombreCards.innerHTML = compteur;
        }
        console.log("ok")
 })
